9f59a2d302c98aa322d378472e1b45d556fc6eab
uuboyscy <aegis12321@gmail.com>
2025-12-20 10:34:41 +0800

    feat: Flask demo

--- flask_demo/app.py
+++ flask_demo/app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, request
+
+from utils import query
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_flask():
-    return "Hello Flask!"
+    return "<h1>Hello Flask!</h1>"
+
+# GET /two_sum/<x>/<y>
+# @app.route("/two_sum/<x>/<y>")
+# def two_sum(x, y):
+#     return str(int(x) + int(y))
+@app.route("/two_sum/<int:x>/<int:y>")
+def two_sum(x, y):
+    return str(x + y)
+
+# REST API, RESTful API
+# GET /api/v1/get-emp?dep_id=123&emp_id=456
+# CRUD
+# Read:     GET /api/v1/emp/<str:dep_id>/<str:emp_id>
+# Create:   POST /api/v1/emp/<str:dep_id>/<str:emp_id> ; body: {token:"123123"}
+# Update:   UPDATE /api/v1/emp/<str:dep_id>/<str:emp_id> ; body: {token:"123123"}
+# Delete:   DELETE /api/v1/emp/<str:dep_id>/<str:emp_id> ; body: {token:"123123"}
+@app.route("/api/v1/emp/<string:dep_id>/<string:emp_id>")
+def emp(dep_id, emp_id):
+    query_sql = f"""
+    select dep_id, emp_id, emp_name from emp
+    where dep_id = '{dep_id}'
+    and emp_id = '{emp_id}'
+    """
+    result = query(query_sql)
+    return result
+
+# GET /hello?username=Allen
+@app.route("/hello")
+def hello():
+    username = request.args.get("username")
+    return f"Hello {username}!"
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(debug=True, host="0.0.0.0", port=5001)
---
+++ flask_demo/utils.py
@@ -0,0 +1,6 @@
+def query(sql) -> dict:
+    return {
+        "dep_id": "aaa",
+        "emp_id": "bbb",
+        "emp_name": "ccc",
+    }
